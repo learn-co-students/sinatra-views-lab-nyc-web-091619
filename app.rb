df56eaa56e3ae91c966ee2c7443a884f1a9e9d15
Liam <liam.healy1@marist.edu>
2019-10-07 13:39:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
 class App < Sinatra::Base
 
+	resp = Rack::Response.new
+
+
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do 
+		resp.status = 200
+		erb :hello
+	end
+
+	get '/goodbye' do
+		resp.status = 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		resp.status = 200
+		erb :date
+	end
+
+
 
 
 end
